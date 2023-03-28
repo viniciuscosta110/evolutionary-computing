683841b81dd1410829819ea9ae767d97860839d9
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-03-28 11:18:33 -0300

    feat: add readme

--- Nqueens/plot.py
+++ Nqueens/plot.py
@@ -4,30 +4,37 @@ from evolutionaryNqueensVariableRatesForIndividual import NqueensIndividual
 
 import matplotlib.pyplot as plt
 
+def print_best_individual(individual):
+    board = [[individual.chromosome[i * 8 + j] for j in range(8)] for i in range(8)]
+    for row in board:
+        print(" ".join(map(str, row)))
+    print()
+
 def main():  
   best_individual, best_value, max_fitness = Nqueens()
   best_individual_adap_pop, best_value_adap_pop, max_fitness_adap_pop = NqueensPopulation()
   best_individual_adap_crom, best_value_adap_crom, max_fitness_adap_crom = NqueensIndividual()
   
-  print(f'Melhor expressão sem adaptação: {best_individual.chromosome} = {best_value}\n')
-  print(f'Melhor expressão com adaptação: {best_individual_adap_pop.chromosome} = {best_value_adap_pop}\n')
-  print(f'Melhor expressão com taxas em cada cromossomo: {best_individual_adap_crom.chromosome} = {best_value_adap_crom}\n')
+  print(f'Best chromossome without adaptation: {print_best_individual(best_individual)} = {best_value}\n')
+  print(f'Best chromossome with adaptation by population : {print_best_individual(best_individual_adap_pop)} = {best_value_adap_pop}\n')
+  print(f'Best chromossome with adaptation in each chromosome: {print_best_individual(best_individual_adap_crom)} = {best_value_adap_crom}\n')
   
-  (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+  fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
 
-  ax1.plot(range(len(max_fitness)), max_fitness, color='blue', label='Sem adaptação')
-  ax1.set_title('Sem adaptação')
+  ax1.plot(range(len(max_fitness)), max_fitness, color='blue', label='Without adaptation')
+  ax1.set_title('Without adaptation')
   ax1.set_ylabel('Fitness')
 
-  ax2.plot(range(len(max_fitness_adap_pop)), max_fitness_adap_pop, color='red', label='Com adaptação')
-  ax2.set_title('Com adaptação por população')
+  ax2.plot(range(len(max_fitness_adap_pop)), max_fitness_adap_pop, color='red', label='With adaptation by population')
+  ax2.set_title('With adaptation by population')
   ax2.set_ylabel('Fitness')
 
-  ax3.plot(range(len(max_fitness_adap_crom)), max_fitness_adap_crom, color='green', label='Com taxas em cada cromossomo')
-  ax3.set_title('Com adaptação em cada cromossomo')
+  ax3.plot(range(len(max_fitness_adap_crom)), max_fitness_adap_crom, color='green', label='With adaptation in each chromosome')
+  ax3.set_title('With adaptation in each chromosome')
   ax3.set_ylabel('Fitness')
-  ax3.set_xlabel('Gerações')
+  ax3.set_xlabel('Generations')
   
+  plt.tight_layout()
   plt.show()
 
 main()
--- Nqueens/readme.md
+++ Nqueens/readme.md
@@ -1,3 +1,7 @@
 # 8 Queens Problem
 
 This folder have an evolutionary and backtracking algorithm to solve the 8 Queens Problem
+
+Also, I implemented another two ones with variable mutation and crossover rates by population and for each chromossome. Here is a graph to show the efficiency of them:
+
+<center> <img title="a title" alt="Alt text" src="../assets/nQueens.png"> </center>
