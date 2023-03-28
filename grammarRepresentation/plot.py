29553221dd04fcb1a16cd1551137c77ff00a7592
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-03-28 11:38:35 -0300

    chore

--- grammarRepresentation/plot.py
+++ grammarRepresentation/plot.py
@@ -16,17 +16,17 @@ def main():
   print(f'Best chromossome with adaptation by population : {(best_individual_adap_pop)} = {best_value_adap_pop}\n')
   print(f'Best chromossome with adaptation in each chromosome: {(best_individual_adap_crom)} = {best_value_adap_crom}\n')
   
-  fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True, )
+  fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
 
-  ax1.plot(range(len(max_fitness)), max_fitness, color='blue', label='Without adaptation')
+  ax1.plot(range(len(max_fitness)), max_fitness, color='blue', label='Without adaptation', marker='o')
   ax1.set_title('Without adaptation')
   ax1.set_ylabel('Fitness')
 
-  ax2.plot(range(len(max_fitness_adap_pop)), max_fitness_adap_pop, color='red', label='With adaptation by population')
+  ax2.plot(range(len(max_fitness_adap_pop)), max_fitness_adap_pop, color='red', label='With adaptation by population', marker='o')
   ax2.set_title('With adaptation by population')
   ax2.set_ylabel('Fitness')
 
-  ax3.plot(range(len(max_fitness_adap_crom)), max_fitness_adap_crom, color='green', label='With adaptation in each chromosome')
+  ax3.plot(range(len(max_fitness_adap_crom)), max_fitness_adap_crom, color='green', label='With adaptation in each chromosome', marker='o')
   ax3.set_title('With adaptation in each chromosome')
   ax3.set_ylabel('Fitness')
   ax3.set_xlabel('Generation')
---
+++ grammarRepresentation/readme.md
@@ -0,0 +1,7 @@
+# Find expression problem
+
+This folder have an evolutionary algorithm to find a expression to represent the target
+
+Also, I implemented another two ones with variable mutation and crossover rates by population and for each chromossome. Here is a graph to show the efficiency of them:
+
+<center> <img title="a title" alt="Alt text" src="../assets/expression.png"> </center>
